25d3c0bcc5d1997317994ed81d1f04b66eb4bf6d
EvilBotCode <tanjinhong62@gmail.com>
2020-12-22 16:22:17 +0800

    simplified version

--- inference.py
+++ inference.py
@@ -8,7 +8,6 @@ import numpy as np
 from datetime import datetime
 import time 
 
-from face_recognition.api import face_distance, face_encodings 
 
 """---------------Functions-----------------"""
 # Function that provides confidence level based on euclidean distance
@@ -36,18 +35,21 @@ def Attendance(name):
 
 
 """------------Code-------------"""
+# Initialize some variables
 Encodings = []
 Names = []
-detected_name_list =[]
+face_names = []
+facePositions = []
 font = cv2.FONT_HERSHEY_DUPLEX
 MODEL = 'hog' 
-
+process_this_frame = True
+acc = 100
 # used to record the time when we processed last frame 
 prev_frame_time = 0
-  
 # used to record the time at which we processed current frame 
 new_frame_time = 0
 
+# Load the encodings and names into the list
 with open('known_faces_feature.pkl','rb') as f:
     Encodings = pickle.load(f)
     Names= pickle.load(f)
@@ -60,74 +62,53 @@ if not cam.isOpened():
     exit()
 
 while True:
-    # Read every frame 
-    face_names = []
+    # Grab a single frame from video
     ret , frame = cam.read()
-    frameSmall = cv2.resize(frame,(0,0),fx=0.33,fy=0.33)
+
+    # Resize the frame to 1/4 for faster face recognition processing
+    frameSmall = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
+
+    # Convert the image from BGR color to RGB color 
     frameRGB = cv2.cvtColor(frameSmall,cv2.COLOR_BGR2RGB)
-    # Calculate fps
-    new_frame_time = time.time()   
-    fps = 1/(new_frame_time-prev_frame_time) 
-    prev_frame_time = new_frame_time 
-    # Display FPS at the Screen
-    cv2.putText(frame, f'{round(fps,1)}', (7, 40), font, 1, (0, 255, 0), 2, cv2.FILLED)
-    facePositions = fr.face_locations(frameRGB, model = MODEL)
-    allEncoding = fr.face_encodings(frameRGB,facePositions)
-    # json_to_export = {} 
-    acc = 100.0
-    for face_encoding in allEncoding:
-        matches = fr.compare_faces(Encodings,face_encoding)
-        name = 'Unknown'
-        # check the known faces with the smallest distance to the new face
-        face_distances = fr.face_distance(Encodings,face_encoding)
-        # Take the best one
-        best_match_index = np.argmin(face_distances)
-        # Name of the best match face
-        name = Names[best_match_index]
 
-        Attendance(name)
-        # Euclidean distance of best match index 
-        Euclidean_dist_best_match = face_distances[best_match_index]
-        # Calculate accuracy of the detection
-        conf = face_distance_to_conf(Euclidean_dist_best_match)
-        acc = conf * 100
-        print(acc)
-        # record the attendance of the best_match face
-        if matches[best_match_index]:
-            # store an instance of the name of detected known face into a list
-            if name not in detected_name_list:
-                detected_name_list.append(name)
-            print(detected_name_list)
+    # Only process everyother frame to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        facePositions = fr.face_locations(frameRGB, model = MODEL)
+        allEncoding = fr.face_encodings(frameRGB,facePositions)
+        
+        face_names = []
+        for face_encoding in allEncoding:
+            matches = fr.compare_faces(Encodings,face_encoding,tolerance = 0.5)
+            name = 'Unknown'
+            # check the known faces with the smallest distance to the new face
+            face_distances = fr.face_distance(Encodings,face_encoding)
+            # Know face with smallest distance to the new face
+            best_match_index = np.argmin(face_distances)
+            # record the attendance of the best_match face
+            if matches[best_match_index]:
+                # Name of the best match face
+                name = Names[best_match_index]
+
             face_names.append(name)
-        # Display the number of known faces detected
-        cv2.putText(frame, f'Known_detected:{len(face_names)}', (340, 40), font, 1, (0, 255, 0), 2, cv2.FILLED)
-            # append the time, name and date of the detected_name_list face to a json dict
-            # json_to_export['Name'] = name
-            # json_to_export['Time'] = f'{time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}'
-            # json_to_export['Date'] = f'{time.localtime().tm_year}-{time.localtime().tm_mon}-{time.localtime().tm_mday}'
-            # Write the information of the detected_name_list face into a csv
-            # with open(f'Attendance/{name.title()}_{time.localtime().tm_year}-{time.localtime().tm_mon}-{time.localtime().tm_mday}_attendance.csv',mode='w') as csv_file:
-            #         fieldnames = ['Name','Time','Date']
-            #         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            #         writer.writeheader()
-            #         writer.writerow({'Name': name.title() , 'Time': f'{time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}', 'Date' : f'{time.localtime().tm_year}-{time.localtime().tm_mon}-{time.localtime().tm_mday}'})
 
-    # Draw the bounding boxes around the identified faces
+    process_this_frame = not process_this_frame
+    # Display the results
     for (top,right,bottom,left), name in zip(facePositions,face_names):
-        top *= 3
-        right *= 3
-        bottom *= 3
-        left *= 3 
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
         
         # Draw a boxes around the face and detected_name_list face label
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
         y = top - 15 if top -15 > 15 else top + 15
         cv2.putText(frame,name.title(),(left,y),font,0.5,(0,255,0),2)
-        # display the acc the accuracy 
-        cv2.putText(frame, f'{round(acc,1)}%', (right, y), font, 0.5, (0, 255, 0), 2)
-    # Output the frame 
+    
+    # Display the resulting image  
     cv2.imshow('Live Video',frame)
     cv2.moveWindow('Live Video',0,0)
+    # Hit Q to quit
     if cv2.waitKey(1) == ord('q'):
         break
 # print(attendance)
