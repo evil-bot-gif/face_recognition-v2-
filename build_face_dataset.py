8ef105c3210890e8b5517ff8faee8ee39a908024
EvilBotCode <tanjinhong62@gmail.com>
2021-01-06 10:10:05 +0800

    matching the png file name with the folder name

--- build_face_dataset.py
+++ build_face_dataset.py
@@ -1,5 +1,6 @@
 from imutils.video import VideoStream
 import imutils
+import face_recognition as fr
 import argparse
 import cv2 
 import time
@@ -8,18 +9,21 @@ import os
 
 # Construct argparse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--cascade", required=True,
-	help = "path to where the face cascade resides")
+# ap.add_argument("-c", "--cascade", required=True,
+# 	help = "path to cv2 haarcascades directory")
+ap.add_argument("-d", "--detection-method", type=str, default="hog",
+	help="face detection model to use: either `hog` or `cnn`")
 ap.add_argument("-o", "--output", required=True,
 	help="path to output directory")
 
 args = vars(ap.parse_args())
 
+# if output directory not available make directory
 if not os.path.exists(args["output"]):
     os.makedirs(args["output"])
 
 # load OpenCV's Haar cascade for face detection from disk
-detector = cv2.CascadeClassifier(cv2.data.haarcascades + args["cascade"])
+# detector = cv2.CascadeClassifier(cv2.data.haarcascades + args["cascade"])
 
 # initialize the video stream, allow the camera sensor to warm up,
 # and initialize the total number of example faces written to disk
@@ -37,15 +41,15 @@ while True:
 	# so we can apply face detection faster
     frame = vs.read()
     orig = frame.copy()
-    frame = imutils.resize(frame,width=480)
+    
 
+    
+    frameRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    rects = fr.face_locations(frameRGB, model = args["detection_method"])
 
-    # detect faces in the grayscale frame 
-    rects = detector.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    # loop over the face detections and draw them on the frame
-    for (x, y, w, h) in rects:
-	    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # draw the bounding box for face detected 
+    for (top,right,bottom,left) in rects:
+	    cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
     
     # show the output frame 
     cv2.imshow("Frame",frame)
@@ -54,7 +58,10 @@ while True:
     # if the 'k' key was pressed, write the "original" frame to disk
     # so we can later process it and used it for face recognition 
     if key == ord('k') :
-        p = os.path.sep.join([args["output"],"{}.png".format(str(total).zfill(5))])
+        # get the basename of the folder where your images are stored in 
+        base_name = os.path.basename(args["output"])
+        # Join the directory with the image file name
+        p = os.path.sep.join([args["output"],f"{base_name+ str(total).zfill(3)}.png"])
         cv2.imwrite(p,orig)
         total += 1
 
@@ -63,8 +70,8 @@ while True:
         break
 
 # print the total faces saved and do a bit of cleanup
-print("[INFO] {} face images stored".format(total))
-print("INFO cleaning up...")
+print(f"[INFO] {total} face images stored ")
+print("[INFO] cleaning up...")
 cv2.destroyAllWindows()
 vs.stop()
    
